552763ff8d2a071e3c1f4126ac8baac0009fc4b6
Jelani Woods <jelani@firstdraft.com>
2022-07-01 16:15:26 -0500

    Second exampel

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+ReactDOM.render(<ul>
+    <li>Buy</li>
+    <li>Gold</li>
+    <li>More</li>
+</ul>, document.getElementById("root"))
